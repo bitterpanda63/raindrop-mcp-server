710d6effcae618c92120e0ffbe1cd2e5d431c301
BitterPanda <bitterpanda@proton.me>
2025-10-01 21:57:38 +0200

    Add index.ts for api

---
+++ src/api/index.ts
@@ -0,0 +1,18 @@
+export { RaindropsAPI } from "./raindrops.js";
+export { CollectionsAPI } from "./collections.js";
+export { TagsAPI } from "./tags.js";
+export { UserAPI } from "./user.js";
+export { HighlightsAPI } from "./highlights.js";
+export { FiltersAPI } from "./filters.js";
+
+export type {
+  Raindrop,
+  GetRaindropsParams,
+  CreateRaindropParams,
+  UpdateRaindropParams
+} from "./raindrops.js";
+export type { Collection, CreateCollectionParams, UpdateCollectionParams } from "./collections.js";
+export type { Tag } from "./tags.js";
+export type { User } from "./user.js";
+export type { Highlight, CreateHighlightParams } from "./highlights.js";
+export type { Filter } from "./filters.js";
